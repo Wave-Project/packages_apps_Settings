fbe773b44d0c5f1e386c1c9cd6ee1827111fad8e
TheStrix <parthbhatia98@gmail.com>
2020-05-28 22:32:10 +0200

    Settings: Implement Battery Styles [2/2]
    Jyotiraditya <jyotiraditya@aospa.co> :
    - Adapted to a11 and cleaned-up
    
    Also run dos2unix on Statusbar.java
    
    Change-Id: Ic41cc9c4720156f5ae78ef9d30791b5d6c0dd09b
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- src/com/android/settings/wave/fragments/Statusbar.java
+++ src/com/android/settings/wave/fragments/Statusbar.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2019 Wave-OS
+ * Copyright (C) 2021 Wave-OS
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,17 +17,60 @@
 package com.android.settings.wave.fragments;
 
 import android.os.Bundle;
-import com.android.settings.SettingsPreferenceFragment;
-import com.android.settings.R;
+import android.provider.Settings;
 
-public class Statusbar extends SettingsPreferenceFragment {
+import androidx.preference.ListPreference;
+import androidx.preference.Preference;
+import androidx.preference.SwitchPreference;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+import static android.provider.Settings.System.SHOW_BATTERY_PERCENT;
+import static android.provider.Settings.System.STATUS_BAR_BATTERY_STYLE;
+
+public class Statusbar extends SettingsPreferenceFragment
+        implements Preference.OnPreferenceChangeListener {
 
     private static final String TAG = "Status bar";
+    private static final String KEY_BATTERY_STYLE = "battery_style";
+    private static final String KEY_BATTERY_PERCENTAGE = "battery_percentage";
+
+    private ListPreference mBatteryStylePref;
+    private SwitchPreference mBatteryPercentagePref;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.statusbar_settings);
+
+        mBatteryStylePref = (ListPreference) findPreference(KEY_BATTERY_STYLE);
+        int value = Settings.System.getInt(getActivity().getContentResolver(), STATUS_BAR_BATTERY_STYLE, 0);
+        mBatteryStylePref.setValue(Integer.toString(value));
+        int index = mBatteryStylePref.findIndexOfValue(Integer.toString(value));
+        mBatteryStylePref.setSummary(mBatteryStylePref.getEntries()[index]);
+        mBatteryStylePref.setOnPreferenceChangeListener(this);
+
+        mBatteryPercentagePref = (SwitchPreference) findPreference(KEY_BATTERY_PERCENTAGE);
+        boolean enabled = Settings.System.getInt(getActivity().getContentResolver(), SHOW_BATTERY_PERCENT, 0) == 1;
+        mBatteryPercentagePref.setChecked(enabled);
+        mBatteryPercentagePref.setOnPreferenceChangeListener(this);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mBatteryStylePref) {
+            int value = Integer.parseInt((String) newValue);
+            int index = mBatteryStylePref.findIndexOfValue((String) newValue);
+            Settings.System.putInt(getActivity().getContentResolver(), STATUS_BAR_BATTERY_STYLE, value);
+            mBatteryStylePref.setSummary(mBatteryStylePref.getEntries()[index]);
+            return true;
+        } else if (preference == mBatteryPercentagePref) {
+            boolean showPercentage = (Boolean) newValue;
+            Settings.System.putInt(getActivity().getContentResolver(), SHOW_BATTERY_PERCENT, showPercentage ? 1 : 0);
+            return true;
+        }
+        return false;
     }
 
     @Override
