e7f29676811cdd7e1cc0b60f8fab1fccb3ee6bda
Jyotiraditya Panda <jyotiraditya@aospa.co>
2021-06-12 00:37:54 +0530

    Settings: Show wave flavour instead of device name
    Signed-off-by: Jyotiraditya Panda <jyotiraditya@aospa.co>

--- src/com/android/settings/deviceinfo/aboutphone/WaveInfoPreferenceController.java
+++ src/com/android/settings/deviceinfo/aboutphone/WaveInfoPreferenceController.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2020 Wave-OS
+ * Copyright (C) 2021 Wave-OS
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -32,46 +32,28 @@ public class WaveInfoPreferenceController extends AbstractPreferenceController {
     private static final String KEY_WAVE_INFO = "wave_info";
 
     private static final String PROP_WAVE_VERSION = "ro.wave.version";
-    private static final String PROP_WAVE_VERSION_CODE = "ro.wave.version_code";
+    private static final String PROP_WAVE_FLAVOUR = "ro.wave.flavour";
     private static final String PROP_WAVE_OFFICIAL = "ro.wave.is_official";
     private static final String PROP_WAVE_MAINTAINER = "ro.wave.maintainer_name";
-    private static final String PROP_WAVE_DEVICE = "ro.wave.device_name";
 
     public WaveInfoPreferenceController(Context context) {
         super(context);
-    }
-
-    private String getDeviceName() {
-        String device = SystemProperties.get(PROP_WAVE_DEVICE, "");
-        if (device.equals("")) {
-            device = Build.MANUFACTURER + " " + Build.MODEL;
-        }
-        return device;
-    }
-
-    private String getWaveVersion() {
-        final String version = SystemProperties.get(PROP_WAVE_VERSION,
-                this.mContext.getString(R.string.device_info_default));
-        final String versionCode = SystemProperties.get(PROP_WAVE_VERSION_CODE,
-                this.mContext.getString(R.string.device_info_default));
-
-        return version + " | " + versionCode;
     }
 
     @Override
     public void displayPreference(PreferenceScreen screen) {
         super.displayPreference(screen);
         final LayoutPreference waveInfoPreference = screen.findPreference(KEY_WAVE_INFO);
-        final TextView version = (TextView) waveInfoPreference.findViewById(R.id.version_message);
-        final TextView device = (TextView) waveInfoPreference.findViewById(R.id.device_message);
-        final TextView maintainer = (TextView) waveInfoPreference.findViewById(R.id.maintainer_message);
-        final TextView releaseType = (TextView) waveInfoPreference.findViewById(R.id.release_type_message);
-        final String waveVersion = getWaveVersion();
-        final String waveDevice = getDeviceName();
-        final String waveMaintainer = SystemProperties.get(PROP_WAVE_MAINTAINER, this.mContext.getString(R.string.device_info_default));
+        final TextView version = waveInfoPreference.findViewById(R.id.version_message);
+        final TextView flavour = waveInfoPreference.findViewById(R.id.flavour_message);
+        final TextView maintainer = waveInfoPreference.findViewById(R.id.maintainer_message);
+        final TextView releaseType = waveInfoPreference.findViewById(R.id.release_type_message);
+        final String waveVersion = SystemProperties.get(PROP_WAVE_VERSION, mContext.getString(R.string.device_info_default));
+        final String waveFlavour = SystemProperties.get(PROP_WAVE_FLAVOUR, mContext.getString(R.string.device_info_default));
+        final String waveMaintainer = SystemProperties.get(PROP_WAVE_MAINTAINER, mContext.getString(R.string.device_info_default));
         final String waveReleaseType = SystemProperties.getBoolean(PROP_WAVE_OFFICIAL, false) ? "Official" : "Unofficial";
         version.setText(waveVersion);
-        device.setText(waveDevice);
+        flavour.setText(waveFlavour);
         maintainer.setText(waveMaintainer);
         releaseType.setText(waveReleaseType);
     }
